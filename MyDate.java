519c6cfb2d98a0abe1bbef589fb7ee7fa965fc1b
Jacob Mueller <jmillthetruth@gmail.com>
2018-09-23 16:15:59 -0500

    Changed leap year, setmydate(string), and getmydatestring() functions

--- MyDate.java
+++ MyDate.java
@@ -1,41 +1,56 @@
 public class MyDate{
 	private int my_date_;
-	
+
 	public MyDate(int date) {
 		SetMyDate(date);
 	}
-	
+	public MyDate(String date){
+		SetMyDate(date);
+	}
+
 	public void SetMyDate(int date) {
 		//assume that the date will be in the format yyyymmdd
 		//date is an integer
 		my_date_ = date;
 	}
-	
+
 	public void SetMyDate(String date) {
 		//assume that the date will be in the format mm/dd/yyyy
-		//my_date_ = 
+		String[] dateArray = date.split("/");
+		String month = dateArray[0];
+		String day = dateArray[1];
+		String year = dateArray[2];
+		String strDate = year + month + day;
+		my_date_ = Integer.parseInt(strDate);
 	}
-	
+
 	public int GetMyDate() {
 		return my_date_;
 	}
-	
+
 	public String GetMyDateString() {
 		//please return a date string in the format yyyymmdd
+		return Integer.toString(my_date_);
 	}
-	
-	public String GetMyDateString1() {
-		//please return a date string in the format of mm/dd/yyyy 
+
+	/*public String GetMyDateString1() {
+		//please return a date string in the format of mm/dd/yyyy
 	}
-	
+
 	public String GetMyDateString2() {
 		//please return a date string in the format of dd/mm/yyyy
 	}
-	
+*/
 	public boolean IsLeapYear(int year) {
 		//please return true if the year is a leap year
 		//return false if the year is not a leap year
+		if(year % 4 == 0){
+            return true;
+		}
+		else{
+            return false;
+		}
 	}
-	
-	
+
+
 }
--- StockSimulation.java
+++ StockSimulation.java
@@ -1,15 +1,16 @@
 public class StockSimulation{
 	public static void main(String[] args) {
 		MyDate date = new MyDate("01/01/2018");
-		
+
 		if(date.IsLeapYear(2018)) {
 			System.out.println("2018 is a leap year");
 		}else {
 			System.out.println("2018 is not a leap year");
 		}
-		
+		System.out.println(date.GetMyDateString());
+
 		Stock s = new Stock("Facebook", "FB", 178.33, date);
-		
+
 		//please check that every get method in the MyDate class is correct
 	}
 }
